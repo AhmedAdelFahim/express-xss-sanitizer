8b506275e3af0be728dfea08072ceb7542f17ae0
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-11-20 14:50:17 +0200

    1.0.0 release

---
+++ README.md
@@ -0,0 +1,38 @@
+# Express XSS Sanitizer
+Express 4.x middleware which sanitizes user input data (in req.body, req.query, req.headers and req.params) to prevent Cross Site Scripting (XSS) attack.
+
+## Installation
+```
+npm install express-xss-sanitizer
+```
+## Usage
+Add as a piece of express middleware, before defining your routes.
+```
+const express = require('express');
+const bodyParser = require('body-parser');
+const xss = require('express-xss-sanitizer');
+
+const app = express();
+
+app.use(bodyParser.json({limit:'1kb'}));
+app.use(bodyParser.urlencoded({extended: true, limit:'1kb'}));
+app.use(xss());
+```
+You can add options to specify allowed keys to be skipped at sanitization
+```
+const options = {
+   allowedKeys: ['name']
+}
+
+app.use(xss(options));
+```
+You can add options to specify allowed tags to sanitize it and remove other tags
+```
+const options = {
+   allowedTags: ['h1']
+}
+
+app.use(xss(options));
+```
+## Support
+Feel free to open issues on [github](https://github.com/AhmedAdelFahim/express-xss-sanitizer.git).
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+const sanitizeHtml = require('sanitize-html');
+
+const initializeOptions = (options) => {
+  const sanitizerOptions = {}
+  if(Array.isArray(options.allowedTags) && options.allowedTags.length > 0) {
+    sanitizerOptions.allowedTags = options.allowedTags;
+  }
+  return {
+    allowedKeys: (Array.isArray(options.allowedKeys) && options.allowedKeys) || [],
+    sanitizerOptions
+  }
+}
+
+const sanitize = (options, data) => {
+  if (typeof data === 'string') {
+    return sanitizeHtml(data, options.sanitizerOptions)
+  } else if (Array.isArray(data)) {
+    return data.map((item)=>{
+      if(typeof item === 'string') {
+        return sanitizeHtml(item, options.sanitizerOptions)
+      } else if(Array.isArray(item) || typeof item === 'object') {
+        return sanitize(options, item)
+      } else {
+        return item
+      }
+    })
+  } else if (typeof data === 'object') {
+    Object.keys(data).forEach((key)=>{
+      if (options.allowedKeys.includes(key)) {
+        return ;
+      }
+      const item = data[key]
+      if(typeof item === 'string') {
+        data[key] = sanitizeHtml(item, options.sanitizerOptions)
+      } else if(Array.isArray(item) || typeof item === 'object') {
+        data[key] = sanitize(options, item)
+      }
+    })
+  }
+  return data
+
+}
+
+function middleware(options={}) {
+  options = initializeOptions(options)
+  console.log(options)
+  return function (req, res, next) {
+    ['body', 'params', 'headers', 'query'].forEach(function (k) {
+      if (req[k]) {
+        req[k] = sanitize(options, req[k]);
+      }
+    });
+    next();
+  };
+}
+
+module.exports = middleware;
